005f47873a8bba9c10331b9eddcd83b52050f04b
Martin Leopold <m@martinleopold.com>
2020-05-12 13:25:19 +0200

    optinal value mapping in `make_point()`

--- Helios.py
+++ Helios.py
@@ -186,10 +186,12 @@ def map_color(r, g, b, i = 1, min = 0, max = 1):
         map_color_comp(i, min, max)
     )
 
-def make_point(x=0, y=0, r=1, g=1, b=1, i=1):
+def make_point(x=0, y=0, r=1, g=1, b=1, i=1, xmin=-1, xmax=1, ymin=None, ymax=None, colormin=0, colormax=1):
+    if ymin is None: ymin = xmin
+    if ymax is None: ymax = xmax
     return HeliosPoint(
-        map_coord(x), map_coord(y),
-        *map_color(r, g, b, i)
+        map_coord(x, xmin, xmax), map_coord(y, ymin, ymax),
+        *map_color(r, g, b, i, colormin, colormax)
     )
 
 if __name__ == '__main__':
